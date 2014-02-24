e29972b88f63479be38c6f98bf9e732feec1e106
Tom Booth <tombooth@gmail.com>
2014-02-24 15:22:02 +0000

    Switch on file logging in collect.py
    This had been switched of during early development and needs to be
    re-enabled.

--- collect.py
+++ collect.py
@@ -1,15 +1,15 @@
-#import logging
-#import os
+import logging
+import os
 
 from backdrop.collector import arguments
-#from backdrop.collector.logging_setup import set_up_logging
+from backdrop.collector.logging_setup import set_up_logging
 
 from collector.trending import compute
 
 if __name__ == '__main__':
-    #logfile_path = os.path.join(
-    #    os.path.dirname(os.path.realpath(__file__)), 'log')
-    #set_up_logging('ga_trending_collector', logging.DEBUG, logfile_path)
+    logfile_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'log')
+    set_up_logging('ga_trending_collector', logging.DEBUG, logfile_path)
 
     args = arguments.parse_args('Google Analytics Trending')
 
